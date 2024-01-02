13055beaacfe90193d6cd59c817f7e8055b258c9
weskerp <iagowesley_nc@hotmail.com>
2024-01-02 14:53:48 -0300

    alter name folder Pages

--- js/login.js
+++ js/login.js
@@ -13,7 +13,7 @@ const validateInput = ({target}) => {
 const handleSubmit = (event) => {
     event.preventDefault();
     localStorage.setItem('player', input.value);
-    window.location = 'pages/game.html';
+    window.location = 'pages-game/game.html';
 }
 
 input.addEventListener('input', validateInput);
